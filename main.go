a378812267fe24b1d0d262aa8eb8b3a629130edc
kamaal111 <kamaal.f1@gmail.com>
2021-09-29 23:01:43 +0200

    Getting scale of images

--- main.go
+++ main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"image"
 	"image/png"
+	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"golang.org/x/image/draw"
 )
 
 func main() {
+	contentsFile, err := ioutil.ReadFile("assets/Contents.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	contentsFileContent := ContentFile{}
+	err = json.Unmarshal([]byte(contentsFile), &contentsFileContent)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, imageItem := range contentsFileContent.Images {
+		sizeValueString := strings.FieldsFunc(imageItem.Size, func(r rune) bool {
+			return r == 'x'
+		})[0]
+		sizeValue, err := strconv.ParseFloat(sizeValueString, 8)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		scaleValueString := strings.FieldsFunc(imageItem.Scale, func(r rune) bool {
+			return r == 'x'
+		})[0]
+		scaleValue, err := strconv.ParseFloat(scaleValueString, 8)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println(sizeValue * scaleValue)
+	}
+
 	input, err := os.Open("logo.png")
 	if err != nil {
 		log.Fatalln(err)
@@ -32,4 +65,21 @@ func main() {
 	png.Encode(output, inputSpecs)
 
 	log.Println("done creating icons")
+}
+
+type ImageItem struct {
+	Filename string `json:"filename"`
+	Idiom    string `json:"idiom"`
+	Scale    string `json:"scale"`
+	Size     string `json:"size"`
+}
+
+type ContentInfo struct {
+	Author  string `json:"author"`
+	Version int    `json:"version"`
+}
+
+type ContentFile struct {
+	Images []ImageItem `json:"images"`
+	Info   ContentInfo `json:"info"`
 }
