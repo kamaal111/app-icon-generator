a64763f799a0bcd1f35c3565c5f836e057b82f4d
kamaal111 <kamaal.f1@gmail.com>
2021-10-02 17:22:55 +0200

    Reading internal string file instead of json file

--- main.go
+++ main.go
@@ -16,8 +16,6 @@ import (
 	"golang.org/x/image/draw"
 )
 
-const CONTENTS_FILENAME = "Contents.json"
-
 func main() {
 	start := time.Now()
 
@@ -36,11 +34,8 @@ func main() {
 		os.Exit(1)
 	}
 
-	contentsFile, err := ioutil.ReadFile(filepath.Join(".", "assets", CONTENTS_FILENAME))
-	checkError(err)
-
 	contentsFileContent := ContentFile{}
-	err = json.Unmarshal([]byte(contentsFile), &contentsFileContent)
+	err := json.Unmarshal([]byte(CONTENTS), &contentsFileContent)
 	checkError(err)
 
 	outputDirectory := filepath.Join(*outputPath, "AppIcon.appiconset")
@@ -50,7 +45,7 @@ func main() {
 	err = os.MkdirAll(outputDirectory, os.ModePerm)
 	checkError(err)
 
-	err = ioutil.WriteFile(filepath.Join(outputDirectory, CONTENTS_FILENAME), contentsFile, 0644)
+	err = ioutil.WriteFile(filepath.Join(outputDirectory, "Contents.json"), []byte(CONTENTS), 0644)
 	checkError(err)
 
 	var createdImageNames []string
@@ -157,3 +152,336 @@ type ContentFile struct {
 	Images []ImageItem `json:"images"`
 	Info   ContentInfo `json:"info"`
 }
+
+const CONTENTS = `
+{
+	"images" : [
+	  {
+		"filename" : "40.png",
+		"idiom" : "iphone",
+		"scale" : "2x",
+		"size" : "20x20"
+	  },
+	  {
+		"filename" : "60.png",
+		"idiom" : "iphone",
+		"scale" : "3x",
+		"size" : "20x20"
+	  },
+	  {
+		"filename" : "29.png",
+		"idiom" : "iphone",
+		"scale" : "1x",
+		"size" : "29x29"
+	  },
+	  {
+		"filename" : "58.png",
+		"idiom" : "iphone",
+		"scale" : "2x",
+		"size" : "29x29"
+	  },
+	  {
+		"filename" : "87.png",
+		"idiom" : "iphone",
+		"scale" : "3x",
+		"size" : "29x29"
+	  },
+	  {
+		"filename" : "80.png",
+		"idiom" : "iphone",
+		"scale" : "2x",
+		"size" : "40x40"
+	  },
+	  {
+		"filename" : "120.png",
+		"idiom" : "iphone",
+		"scale" : "3x",
+		"size" : "40x40"
+	  },
+	  {
+		"filename" : "57.png",
+		"idiom" : "iphone",
+		"scale" : "1x",
+		"size" : "57x57"
+	  },
+	  {
+		"filename" : "114.png",
+		"idiom" : "iphone",
+		"scale" : "2x",
+		"size" : "57x57"
+	  },
+	  {
+		"filename" : "120.png",
+		"idiom" : "iphone",
+		"scale" : "2x",
+		"size" : "60x60"
+	  },
+	  {
+		"filename" : "180.png",
+		"idiom" : "iphone",
+		"scale" : "3x",
+		"size" : "60x60"
+	  },
+	  {
+		"filename" : "20.png",
+		"idiom" : "ipad",
+		"scale" : "1x",
+		"size" : "20x20"
+	  },
+	  {
+		"filename" : "40.png",
+		"idiom" : "ipad",
+		"scale" : "2x",
+		"size" : "20x20"
+	  },
+	  {
+		"filename" : "29.png",
+		"idiom" : "ipad",
+		"scale" : "1x",
+		"size" : "29x29"
+	  },
+	  {
+		"filename" : "58.png",
+		"idiom" : "ipad",
+		"scale" : "2x",
+		"size" : "29x29"
+	  },
+	  {
+		"filename" : "40.png",
+		"idiom" : "ipad",
+		"scale" : "1x",
+		"size" : "40x40"
+	  },
+	  {
+		"filename" : "80.png",
+		"idiom" : "ipad",
+		"scale" : "2x",
+		"size" : "40x40"
+	  },
+	  {
+		"filename" : "50.png",
+		"idiom" : "ipad",
+		"scale" : "1x",
+		"size" : "50x50"
+	  },
+	  {
+		"filename" : "100.png",
+		"idiom" : "ipad",
+		"scale" : "2x",
+		"size" : "50x50"
+	  },
+	  {
+		"filename" : "72.png",
+		"idiom" : "ipad",
+		"scale" : "1x",
+		"size" : "72x72"
+	  },
+	  {
+		"filename" : "144.png",
+		"idiom" : "ipad",
+		"scale" : "2x",
+		"size" : "72x72"
+	  },
+	  {
+		"filename" : "76.png",
+		"idiom" : "ipad",
+		"scale" : "1x",
+		"size" : "76x76"
+	  },
+	  {
+		"filename" : "152.png",
+		"idiom" : "ipad",
+		"scale" : "2x",
+		"size" : "76x76"
+	  },
+	  {
+		"filename" : "167.png",
+		"idiom" : "ipad",
+		"scale" : "2x",
+		"size" : "83.5x83.5"
+	  },
+	  {
+		"filename" : "1024.png",
+		"idiom" : "ios-marketing",
+		"scale" : "1x",
+		"size" : "1024x1024"
+	  },
+	  {
+		"filename" : "48.png",
+		"idiom" : "watch",
+		"role" : "notificationCenter",
+		"scale" : "2x",
+		"size" : "24x24",
+		"subtype" : "38mm"
+	  },
+	  {
+		"filename" : "55.png",
+		"idiom" : "watch",
+		"role" : "notificationCenter",
+		"scale" : "2x",
+		"size" : "27.5x27.5",
+		"subtype" : "42mm"
+	  },
+	  {
+		"filename" : "58.png",
+		"idiom" : "watch",
+		"role" : "companionSettings",
+		"scale" : "2x",
+		"size" : "29x29"
+	  },
+	  {
+		"filename" : "87.png",
+		"idiom" : "watch",
+		"role" : "companionSettings",
+		"scale" : "3x",
+		"size" : "29x29"
+	  },
+	  {
+		"idiom" : "watch",
+		"role" : "notificationCenter",
+		"scale" : "2x",
+		"size" : "33x33",
+		"subtype" : "45mm"
+	  },
+	  {
+		"filename" : "80.png",
+		"idiom" : "watch",
+		"role" : "appLauncher",
+		"scale" : "2x",
+		"size" : "40x40",
+		"subtype" : "38mm"
+	  },
+	  {
+		"filename" : "88.png",
+		"idiom" : "watch",
+		"role" : "appLauncher",
+		"scale" : "2x",
+		"size" : "44x44",
+		"subtype" : "40mm"
+	  },
+	  {
+		"idiom" : "watch",
+		"role" : "appLauncher",
+		"scale" : "2x",
+		"size" : "46x46",
+		"subtype" : "41mm"
+	  },
+	  {
+		"filename" : "100.png",
+		"idiom" : "watch",
+		"role" : "appLauncher",
+		"scale" : "2x",
+		"size" : "50x50",
+		"subtype" : "44mm"
+	  },
+	  {
+		"idiom" : "watch",
+		"role" : "appLauncher",
+		"scale" : "2x",
+		"size" : "51x51",
+		"subtype" : "45mm"
+	  },
+	  {
+		"filename" : "172.png",
+		"idiom" : "watch",
+		"role" : "quickLook",
+		"scale" : "2x",
+		"size" : "86x86",
+		"subtype" : "38mm"
+	  },
+	  {
+		"filename" : "196.png",
+		"idiom" : "watch",
+		"role" : "quickLook",
+		"scale" : "2x",
+		"size" : "98x98",
+		"subtype" : "42mm"
+	  },
+	  {
+		"filename" : "216.png",
+		"idiom" : "watch",
+		"role" : "quickLook",
+		"scale" : "2x",
+		"size" : "108x108",
+		"subtype" : "44mm"
+	  },
+	  {
+		"idiom" : "watch",
+		"role" : "quickLook",
+		"scale" : "2x",
+		"size" : "117x117",
+		"subtype" : "45mm"
+	  },
+	  {
+		"filename" : "1024.png",
+		"idiom" : "watch-marketing",
+		"scale" : "1x",
+		"size" : "1024x1024"
+	  },
+	  {
+		"filename" : "16.png",
+		"idiom" : "mac",
+		"scale" : "1x",
+		"size" : "16x16"
+	  },
+	  {
+		"filename" : "32.png",
+		"idiom" : "mac",
+		"scale" : "2x",
+		"size" : "16x16"
+	  },
+	  {
+		"filename" : "32.png",
+		"idiom" : "mac",
+		"scale" : "1x",
+		"size" : "32x32"
+	  },
+	  {
+		"filename" : "64.png",
+		"idiom" : "mac",
+		"scale" : "2x",
+		"size" : "32x32"
+	  },
+	  {
+		"filename" : "128.png",
+		"idiom" : "mac",
+		"scale" : "1x",
+		"size" : "128x128"
+	  },
+	  {
+		"filename" : "256.png",
+		"idiom" : "mac",
+		"scale" : "2x",
+		"size" : "128x128"
+	  },
+	  {
+		"filename" : "256.png",
+		"idiom" : "mac",
+		"scale" : "1x",
+		"size" : "256x256"
+	  },
+	  {
+		"filename" : "512.png",
+		"idiom" : "mac",
+		"scale" : "2x",
+		"size" : "256x256"
+	  },
+	  {
+		"filename" : "512.png",
+		"idiom" : "mac",
+		"scale" : "1x",
+		"size" : "512x512"
+	  },
+	  {
+		"filename" : "1024.png",
+		"idiom" : "mac",
+		"scale" : "2x",
+		"size" : "512x512"
+	  }
+	],
+	"info" : {
+	  "author" : "xcode",
+	  "version" : 1
+	}
+  }
+`
